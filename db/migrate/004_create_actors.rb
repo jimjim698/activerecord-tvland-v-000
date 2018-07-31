6da32ddca9781807d14952e018b0b3f84e07a8eb
James Lotruglio <jimlotruglio@yahoo.com>
2018-07-31 03:00:36 +0000

    automatically backed up by learn

---
+++ db/migrate/004_create_actors.rb
@@ -0,0 +1,11 @@
+class Actors < ActiveRecord::Migration 
+
+def change 
+  create_table :actors do |t|
+    t.string :first_name 
+    t.string :last_name
+  end 
+end 
+
+  
+end 
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,4 @@
+class CreateCharacters < ActiveRecord::Migration
+
+
+end 
