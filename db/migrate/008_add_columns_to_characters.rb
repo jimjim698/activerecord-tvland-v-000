a1ec0fe612b38677abbb0be27f76c42e0e9e8200
James Lotruglio <jimlotruglio@yahoo.com>
2018-07-31 03:30:39 +0000

    automatically backed up by learn

--- db/migrate/008_add_columns_to_characters.rb
+++ db/migrate/008_add_columns_to_characters.rb
@@ -2,7 +2,7 @@ class AddColumnsToCharacters < ActiveRecord::Migration[5.1]
 
   def change
     add_column :characters, :show_id, :integer
-    add_column :characters, :artist_id, :integer 
+    add_column :characters, :actor_id, :integer
   end
 
 end 
